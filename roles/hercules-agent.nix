3e8f9880aa5eaa91a4c31f38d2f06df7fca4050f
John Lotoski <john.lotoski@iohk.io>
2020-03-18 11:39:47 -0400

    Adjust hercules concurrency to 3

--- roles/hercules-agent.nix
+++ roles/hercules-agent.nix
@@ -3,7 +3,7 @@
 in {
   imports = [ (sources.hercules-ci-agent + "/module.nix") ];
   services.hercules-ci-agent.enable = true;
-  services.hercules-ci-agent.concurrentTasks = 4;
+  services.hercules-ci-agent.concurrentTasks = 3;
   deployment.keys."cluster-join-token.key".keyFile = ../secrets/hercules-ci-token.key;
   deployment.keys."binary-caches.json".keyFile = ../secrets/cachix-binary-caches.json;
 }

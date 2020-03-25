12c774be73c28c8f6ffaf7942b4e7d916f9de267
John Lotoski <john.lotoski@iohk.io>
2020-03-25 10:48:26 -0400

    Adjust release.nix for upstream impure fetcher; WIP

--- release.nix
+++ release.nix
@@ -8,6 +8,13 @@ in pkgs.lib.fix (self: {
   signing = (import ./nix-darwin/test.nix { role = "signing"; host = "build"; port = "123"; hostname = "hostname"; }).system;
   required = pkgs.releaseTools.aggregate {
     name = "required";
-    constituents = with self; [ ci buildkite-agent hydra-slave signing ];
+    constituents = with self; [
+      # Re-add once upstream impure hercules fetcher is fixed:
+      # error: access to URI 'https://github.com/hercules-ci/gitignore/archive/f9e996052b5af4032fe6150bba4a6fe4f7b9d698.tar.gz' is forbidden in restricted mode
+      #ci
+      #hydra-slave
+      buildkite-agent
+      signing
+    ];
   };
 })

a1671272fb09d3ab99c94efcf2c980e6117feb2e
John Lotoski <john.lotoski@iohk.io>
2020-04-09 10:44:35 -0400

    Migrate to hydra iohk-master

--- pkgs/hydra.nix
+++ pkgs/hydra.nix
@@ -8,14 +8,14 @@ let
 
   # Step 0 -- At legacy (migration tested locally from this pin)
   #
-  hydraSrc = sources.hydra-legacy;
-  hydraNixpkgs = pkgs;
-  hydraPatches = [
-    (pkgs.fetchpatch {
-      url = "https://github.com/NixOS/hydra/pull/648/commits/4171ab4c4fd576c516dc03ba64d1c7945f769af0.patch";
-      sha256 = "1fxa2459kdws6qc419dv4084c1ssmys7kqg4ic7n643kybamsgrx";
-    })
-  ];
+  #hydraSrc = sources.hydra-legacy;
+  #hydraNixpkgs = pkgs;
+  #hydraPatches = [
+  #  (pkgs.fetchpatch {
+  #    url = "https://github.com/NixOS/hydra/pull/648/commits/4171ab4c4fd576c516dc03ba64d1c7945f769af0.patch";
+  #    sha256 = "1fxa2459kdws6qc419dv4084c1ssmys7kqg4ic7n643kybamsgrx";
+  #  })
+  #];
 
   # Step 1 -- Move to rev add4f6 upstream
   #
@@ -25,9 +25,9 @@ let
 
   # Step 2 -- Final commit; move to upstream master + IOHK customization cherry picks
   #
-  #hydraSrc = sources.hydra;
-  #hydraNixpkgs = import sources.hydra-nixpkgs { };
-  #hydraPatches = [ ];
+  hydraSrc = sources.hydra;
+  hydraNixpkgs = import sources.hydra-nixpkgs { };
+  hydraPatches = [ ];
 in
   hydraNixpkgs.callPackage ./hydra-fork.nix {
     nixpkgsPath = hydraNixpkgs.path;

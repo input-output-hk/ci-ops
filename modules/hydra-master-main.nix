b3810b4c3a243bc2f80d822596cddc8d78e2e253
John Lotoski <john.lotoski@iohk.io>
2020-04-28 17:23:23 -0400

    Hydra migration clean up and concurrency/cache update

--- modules/hydra-master-main.nix
+++ modules/hydra-master-main.nix
@@ -69,7 +69,7 @@ in {
       ((mkMac "mac-mini-2") // { speedFactor = 2; })
       localMachine
     ];
-    binaryCaches = mkForce [ "https://cache.nixos.org" ];
+    binaryCaches = mkForce [ "https://cache.nixos.org" "https://hydra.iohk.io" ];
   };
 
   systemd.services.hydra-evaluator.environment.GC_INITIAL_HEAP_SIZE = toString (1024*1024*1024*5); # 5gig
@@ -83,7 +83,7 @@ in {
       evaluator_max_heap_size = ${toString (5 * 1024 * 1024 * 1024)}
       max_db_connections = 50
 
-      max_concurrent_evals = 4
+      max_concurrent_evals = 8
 
       store_uri = s3://iohk-nix-cache?secret-key=/etc/nix/hydra.iohk.io-1/secret&log-compression=br&region=eu-central-1
       server_store_uri = https://iohk-nix-cache.s3-eu-central-1.amazonaws.com/
--- pkgs/hydra.nix
+++ pkgs/hydra.nix
@@ -3,28 +3,6 @@
 let
   sources = import ../nix/sources.nix;
 
-  # Migration route from hydra legacy (19.03) to master:
-  # Rough outline at: https://github.com/NixOS/hydra/issues/725
-
-  # Step 0 -- At legacy (migration tested locally from this pin)
-  #
-  #hydraSrc = sources.hydra-legacy;
-  #hydraNixpkgs = pkgs;
-  #hydraPatches = [
-  #  (pkgs.fetchpatch {
-  #    url = "https://github.com/NixOS/hydra/pull/648/commits/4171ab4c4fd576c516dc03ba64d1c7945f769af0.patch";
-  #    sha256 = "1fxa2459kdws6qc419dv4084c1ssmys7kqg4ic7n643kybamsgrx";
-  #  })
-  #];
-
-  # Step 1 -- Move to rev add4f6 upstream
-  #
-  #hydraSrc = sources.hydra-migrate-step-1;
-  #hydraNixpkgs = import sources.hydra-migrate-nixpkgs-step-1 { };
-  #hydraPatches = [ ];
-
-  # Step 2 -- Final commit; move to upstream master + IOHK customization cherry picks
-  #
   hydraSrc = sources.hydra;
   hydraNixpkgs = import sources.hydra-nixpkgs { };
   hydraPatches = [ ];

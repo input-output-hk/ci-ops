e38f70b5552ed14aa59239b6857b26af1f9962f4
John Lotoski <john.lotoski@iohk.io>
2021-03-25 15:46:08 -0400

    Bump linux hercules for testing

--- roles/hercules-agent.nix
+++ roles/hercules-agent.nix
@@ -2,8 +2,14 @@
   sources = import ../nix/sources.nix;
 in {
   imports = [ (sources.hercules-ci-agent + "/module.nix") ];
-  services.hercules-ci-agent.enable = true;
-  services.hercules-ci-agent.concurrentTasks = 3;
+  services.hercules-ci-agent = {
+    enable = true;
+    concurrentTasks = 1;
+    patchNix = true;
+    extraOptions = {
+      logLevel = "DebugS";
+    };
+  };
   deployment.keys."cluster-join-token.key".keyFile = ../secrets/hercules-ci-token.key;
   deployment.keys."binary-caches.json".keyFile = ../secrets/cachix-binary-caches.json;
 }

8805161355af6a453dde073214157054a66d7100
Michael Bishop <cleverca22@gmail.com>
2021-11-22 12:51:23 -0400

    fix reference to cachecache

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     hydra.url = "github:input-output-hk/hydra/iohk-master-rebased";
+    cachecache.url = "github:cleverca22/cachecache";
   };
   outputs = { ... }: {
   };
--- pkgs/hydra.nix
+++
@@ -1,4 +0,0 @@
-let
-  self = builtins.getFlake (toString ./..);
-in
-  self.inputs.hydra.outputs.packages.x86_64-linux.hydra

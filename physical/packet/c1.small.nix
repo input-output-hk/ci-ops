382fbf9f59d123ac01a6316ced1eace48a4539bd
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2022-02-09 10:45:01 +0100

    Use grub on c1.small

--- physical/packet/c1.small.nix
+++ physical/packet/c1.small.nix
@@ -1,4 +1,10 @@
 { ... }: {
   imports = [ ./. ];
   deployment.packet.plan = "c1.small.x86";
+  boot.loader.grub = {
+    efiSupport = false;
+    enable = true;
+    version = 2;
+    device = "/dev/sda";
+  };
 }

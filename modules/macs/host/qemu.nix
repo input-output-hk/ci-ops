3bcaf9a0bcb27da6edb8bbae447bb4d842459690
Timothy DeHerrera <tim.deherrera@iohk.io>
2022-03-21 08:29:27 -0600

    update Mac images to Monterey
    * Use OVMF images directly from upstream OSX-KVM repo
    * refactor launch script based on upstream
    * Switch from Clover to OpenCore
    * Remove unecessary module options

--- modules/macs/host/qemu.nix
+++ modules/macs/host/qemu.nix
@@ -2,7 +2,7 @@
 { config, lib, pkgs, ... }:
 let
   inherit (config.macosGuest.guest) threads cores sockets memoryInMegs
-    ovmfCodeFile ovmfVarsFile cloverImage snapshotName;
+    snapshotName;
   inherit (lib) mkIf;
 
 in {
@@ -13,7 +13,7 @@ in {
       snapshot = "${zvolName}@${snapshotName}";
       clonedVol = "${zvolName}-${key}";
       clonedZvolDevice = "/dev/zvol/${clonedVol}";
-      inherit (value.guest) cores threads sockets memoryInMegs ovmfCodeFile ovmfVarsFile cloverImage MACAddress;
+      inherit (value.guest) cores threads sockets memoryInMegs MACAddress;
       deps = [ "create-macos-secrets-${key}.service" "dhcpd4.service" "kresd@.service" "network-online.target" ];
     in [ {
       name = "run-macos-vm-${key}";
@@ -41,42 +41,65 @@ in {
           cp -r ${value.guest.persistentConfigDir} /tmp/cdr
           rsync -a ${value.guest.guestConfigDir}/ /tmp/cdr
           cd /tmp/cdr
+          chmod +x apply.sh
           find .
           genisoimage -v -J -r -V CONFIG -o /tmp/config.iso .
         '';
         postStop = ''
           echo poststop script
+          rm -rf /tmp/OSX-KVM
+
           while [ -e /dev/${clonedVol} ]
           do
             zfs destroy ${clonedVol} || (echo "waiting for volume to finish removing" ; sleep 1)
           done
         '';
-        script = ''
-          echo main script
-          exec qemu-system-x86_64 \
-              -enable-kvm \
-              -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,+aes,+xsave,+avx,+xsaveopt,avx2,+smep \
-              -machine pc-q35-2.9 \
-              -smp cpus=${toString (cores * threads * sockets)},cores=${toString cores},threads=${toString threads},sockets=${toString sockets} \
-              -m ${toString memoryInMegs} \
-              -usb -device usb-kbd -device usb-tablet \
-              -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
-              -drive if=pflash,format=raw,readonly,file=${ovmfCodeFile} \
-              -drive if=pflash,format=raw,snapshot=on,file=${ovmfVarsFile} \
-              -smbios type=2 \
-              -device ich9-intel-hda -device hda-duplex \
-              -device ich9-ahci,id=sata \
-              -device ide-hd,bus=sata.0,drive=Clover \
-              -drive id=Clover,if=none,snapshot=on,format=qcow2,file='${cloverImage}' \
-              -device ide-hd,bus=sata.1,drive=MacHDD \
-              -drive id=MacHDD,cache=unsafe,if=none,file=${clonedZvolDevice},format=raw \
-              -device ide-cd,bus=sata.2,drive=config \
-              -drive id=config,if=none,snapshot=on,media=cdrom,file=/tmp/config.iso \
-              -netdev tap,id=net0,ifname=tap-${key},script=no,downscript=no -device e1000-82545em,netdev=net0,id=net0,mac=${MACAddress} \
-              -global PIIX4_PM.disable_s3=1 -global PIIX4_PM.disable_s5=1 \
-              -vnc ${value.guest.vncListen} \
-              -spice port=${toString value.guest.spicePort},addr=0.0.0.0,image-compression=auto_glz,playback-compression=off,agent-mouse=on,zlib-glz-wan-compression=never,jpeg-wan-compression=never,seamless-migration=on,disable-ticketing=on,plaintext-channel=main,plaintext-channel=display,plaintext-channel=inputs,plaintext-channel=cursor,plaintext-channel=playback,plaintext-channel=record,plaintext-channel=usbredir,streaming-video=filter \
-              -monitor unix:/tmp/monitor-socket,server,nowait
+
+        script = let
+          osxKVM = pkgs.fetchFromGitHub {
+            owner = "kholia";
+            repo = "OSX-KVM";
+            rev = "670cd80d7b011447453152feeae5ccf637250f8f";
+            sha256 = "14f49yvv53jj2vkvcbfqnzcww8fq0hc40r9sxs49m20ghdylyh91";
+          };
+        in ''
+          cp -r ${osxKVM} /tmp/OSX-KVM
+          chmod 644 -R /tmp/OSX-KVM
+
+          MY_OPTIONS="+ssse3,+sse4.2,+popcnt,+avx,+aes,+avx2,+smep,+xsave,+xsaveopt,check"
+
+          REPO_PATH="/tmp/OSX-KVM"
+          OVMF_DIR="."
+
+          # shellcheck disable=SC2054
+          args=(
+            -enable-kvm -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,"$MY_OPTIONS"
+            -machine q35
+            -smp cpus=${toString (cores * threads * sockets)},cores=${toString cores},threads=${toString threads},sockets=${toString sockets} \
+            -m ${toString memoryInMegs} \
+            -usb -device usb-kbd -device usb-tablet
+            -device usb-ehci,id=ehci
+            -device nec-usb-xhci,id=xhci
+            -global nec-usb-xhci.msi=off
+            -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+            -drive if=pflash,format=raw,readonly=on,file="$REPO_PATH/$OVMF_DIR/OVMF_CODE.fd"
+            -drive if=pflash,format=raw,file="$REPO_PATH/$OVMF_DIR/OVMF_VARS-1024x768.fd"
+            -smbios type=2
+            -device ich9-intel-hda -device hda-duplex
+            -device ich9-ahci,id=sata
+            -drive id=OpenCoreBoot,if=none,snapshot=on,format=qcow2,file="$REPO_PATH/OpenCore/OpenCore.qcow2"
+            -device ide-hd,bus=sata.2,drive=OpenCoreBoot
+            -drive id=MacHDD,cache=unsafe,if=none,file=${clonedZvolDevice},format=raw
+            -device ide-hd,bus=sata.4,drive=MacHDD
+            -device ide-cd,bus=sata.3,drive=config
+            -drive id=config,if=none,snapshot=on,media=cdrom,file=/tmp/config.iso
+            -netdev tap,id=net0,ifname=tap-${key},script=no,downscript=no -device e1000-82545em,netdev=net0,id=net0,mac=${MACAddress} \
+            -vnc ${value.guest.vncListen} \
+            -spice port=${toString value.guest.spicePort},addr=0.0.0.0,image-compression=auto_glz,playback-compression=off,agent-mouse=on,zlib-glz-wan-compression=never,jpeg-wan-compression=never,seamless-migration=on,disable-ticketing=on,plaintext-channel=main,plaintext-channel=display,plaintext-channel=inputs,plaintext-channel=cursor,plaintext-channel=playback,plaintext-channel=record,plaintext-channel=usbredir,streaming-video=filter \
+            -monitor unix:/tmp/monitor-socket,server,nowait
+          )
+
+          qemu-system-x86_64 "''${args[@]}"
         '';
       };
     }
